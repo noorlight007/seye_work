59a07eb87e6f7d6c18a315936b5963bd8a22659c
noorlight007 <contactwnoor@gmail.com>
2024-12-26 01:55:20 +0600

    update

--- sc_web.py
+++ sc_web.py
@@ -11,37 +11,17 @@ from bs4 import BeautifulSoup
 
 from db_manage import create_new_property
 
+under_const = ["https://sabluximmobilier.com/property_status/en-construction/?advanced_contystate=cote-divoire&filter_search_action%5B0%5D=appartements&submit=Recherche&elementor_form_id=18618",
+                 "https://sabluximmobilier.com/property_status/en-construction/page/2/?advanced_contystate=guinee-conakry&filter_search_action%5B0%5D=terrains&submit=Recherche&elementor_form_id=18618",
+                 "https://sabluximmobilier.com/property_status/en-construction/page/3/?advanced_contystate=guinee-conakry&filter_search_action%5B0%5D=terrains&submit=Recherche&elementor_form_id=18618"]
 
-senegal_links= ["https://sabluximmobilier.com/?advanced_contystate=senegal&filter_search_action%5B0%5D=appartements&property_status=&submit=Recherche&elementor_form_id=18618",
-                "https://sabluximmobilier.com/page/2/?advanced_contystate=senegal&filter_search_action%5B0%5D=appartements&property_status&submit=Recherche&elementor_form_id=18618",
-                "https://sabluximmobilier.com/?advanced_contystate=senegal&filter_search_action%5B0%5D=plateaux-de-bureau&property_status&submit=Recherche&elementor_form_id=18618",
-                "https://sabluximmobilier.com/page/2/?advanced_contystate=senegal&filter_search_action%5B0%5D=plateaux-de-bureau&property_status&submit=Recherche&elementor_form_id=18618",
-                "https://sabluximmobilier.com/?advanced_contystate=senegal&filter_search_action%5B%5D=terrains&property_status=&submit=Recherche&elementor_form_id=18618",
-                "https://sabluximmobilier.com/page/2/?advanced_contystate=senegal&filter_search_action%5B0%5D=terrains&property_status&submit=Recherche&elementor_form_id=18618",
-                "https://sabluximmobilier.com/?advanced_contystate=senegal&filter_search_action%5B%5D=villa&property_status=&submit=Recherche&elementor_form_id=18618",
-                "https://sabluximmobilier.com/page/2/?advanced_contystate=senegal&filter_search_action%5B0%5D=villa&property_status&submit=Recherche&elementor_form_id=18618"]
+launch_list = ["https://sabluximmobilier.com/property_status/lancement/?advanced_contystate=cote-divoire&filter_search_action%5B0%5D&submit=Recherche&elementor_form_id=18618",
+                 "https://sabluximmobilier.com/property_status/lancement/page/2/?advanced_contystate=cote-divoire&filter_search_action%5B0%5D&submit=Recherche&elementor_form_id=18618"]
 
-guinee_conakry= ["https://sabluximmobilier.com/?advanced_contystate=guinee-conakry&filter_search_action%5B0%5D=appartements&property_status=&submit=Recherche&elementor_form_id=18618",
-                "https://sabluximmobilier.com/page/2/?advanced_contystate=guinee-conakry&filter_search_action%5B0%5D=appartements&property_status&submit=Recherche&elementor_form_id=18618",
-                "https://sabluximmobilier.com/?advanced_contystate=guinee-conakry&filter_search_action%5B0%5D=plateaux-de-bureau&property_status&submit=Recherche&elementor_form_id=18618",
-                "https://sabluximmobilier.com/page/2/?advanced_contystate=guinee-conakry&filter_search_action%5B0%5D=plateaux-de-bureau&property_status&submit=Recherche&elementor_form_id=18618",
-                "https://sabluximmobilier.com/?advanced_contystate=guinee-conakry&filter_search_action%5B%5D=terrains&property_status=&submit=Recherche&elementor_form_id=18618",
-                "https://sabluximmobilier.com/page/2/?advanced_contystate=guinee-conakry&filter_search_action%5B0%5D=terrains&property_status&submit=Recherche&elementor_form_id=18618",
-                "https://sabluximmobilier.com/?advanced_contystate=guinee-conakry&filter_search_action%5B%5D=villa&property_status=&submit=Recherche&elementor_form_id=18618",
-                "https://sabluximmobilier.com/page/2/?advanced_contystate=guinee-conakry&filter_search_action%5B0%5D=villa&property_status&submit=Recherche&elementor_form_id=18618"]
-
-
-cote_divoire= ["https://sabluximmobilier.com/?advanced_contystate=cote-divoire&filter_search_action%5B0%5D=appartements&property_status=&submit=Recherche&elementor_form_id=18618",
-                "https://sabluximmobilier.com/page/2/?advanced_contystate=cote-divoire&filter_search_action%5B0%5D=appartements&property_status&submit=Recherche&elementor_form_id=18618",
-                "https://sabluximmobilier.com/?advanced_contystate=cote-divoire&filter_search_action%5B0%5D=plateaux-de-bureau&property_status&submit=Recherche&elementor_form_id=18618",
-                "https://sabluximmobilier.com/page/2/?advanced_contystate=cote-divoire&filter_search_action%5B0%5D=plateaux-de-bureau&property_status&submit=Recherche&elementor_form_id=18618",
-                "https://sabluximmobilier.com/?advanced_contystate=cote-divoire&filter_search_action%5B%5D=terrains&property_status=&submit=Recherche&elementor_form_id=18618",
-                "https://sabluximmobilier.com/page/2/?advanced_contystate=cote-divoire&filter_search_action%5B0%5D=terrains&property_status&submit=Recherche&elementor_form_id=18618",
-                "https://sabluximmobilier.com/?advanced_contystate=cote-divoire&filter_search_action%5B%5D=villa&property_status=&submit=Recherche&elementor_form_id=18618",
-                "https://sabluximmobilier.com/page/2/?advanced_contystate=cote-divoire&filter_search_action%5B0%5D=villa&property_status&submit=Recherche&elementor_form_id=18618"]
-
-
-
+delivered_list = ["https://sabluximmobilier.com/property_status/livre/?advanced_contystate=cote-divoire&filter_search_action%5B0%5D&submit=Recherche&elementor_form_id=18618",
+                  "https://sabluximmobilier.com/property_status/livre/page/2/?advanced_contystate=cote-divoire&filter_search_action%5B0%5D&submit=Recherche&elementor_form_id=18618",
+                  "https://sabluximmobilier.com/property_status/livre/page/3/?advanced_contystate=cote-divoire&filter_search_action%5B0%5D&submit=Recherche&elementor_form_id=18618",
+                  ]
 
 
 
@@ -54,8 +34,8 @@ def scrape_sablux_properties():
     chrome_options.add_argument("--no-sandbox")  # Run in sandbox mode
     chrome_options.add_argument("--disable-dev-shm-usage")  # Disable /dev/shm usage for large pages
 
-    ## Senegal Properties
-    for url in senegal_links:
+    ## under_const Properties
+    for url in under_const:
         service = Service("/usr/local/bin/chromedriver")  # Update this to your chromedriver path
         driver = webdriver.Chrome(service=service, options=chrome_options)
         # Open the webpage
@@ -99,8 +79,8 @@ def scrape_sablux_properties():
             create_new_property(property_name, property_type, property_status, price, property_link)
         time.sleep(5)
 
-    ## guinee_conakry
-    for url in guinee_conakry:
+    ## launch_list
+    for url in launch_list:
         service = Service("/usr/local/bin/chromedriver")  # Update this to your chromedriver path
         driver = webdriver.Chrome(service=service, options=chrome_options)
         # Open the webpage
@@ -144,8 +124,8 @@ def scrape_sablux_properties():
             create_new_property(property_name, property_type, property_status, price, property_link)
         time.sleep(5)
 
-    ## cote_divoire
-    for url in cote_divoire:
+    ## delivered_list
+    for url in delivered_list:
         service = Service("/usr/local/bin/chromedriver")  # Update this to your chromedriver path
         driver = webdriver.Chrome(service=service, options=chrome_options)
         # Open the webpage
---
+++ trash_code.py
@@ -0,0 +1,27 @@
+# senegal_links= ["https://sabluximmobilier.com/?advanced_contystate=senegal&filter_search_action%5B0%5D=appartements&property_status=&submit=Recherche&elementor_form_id=18618",
+#                 "https://sabluximmobilier.com/page/2/?advanced_contystate=senegal&filter_search_action%5B0%5D=appartements&property_status&submit=Recherche&elementor_form_id=18618",
+#                 "https://sabluximmobilier.com/?advanced_contystate=senegal&filter_search_action%5B0%5D=plateaux-de-bureau&property_status&submit=Recherche&elementor_form_id=18618",
+#                 "https://sabluximmobilier.com/page/2/?advanced_contystate=senegal&filter_search_action%5B0%5D=plateaux-de-bureau&property_status&submit=Recherche&elementor_form_id=18618",
+#                 "https://sabluximmobilier.com/?advanced_contystate=senegal&filter_search_action%5B%5D=terrains&property_status=&submit=Recherche&elementor_form_id=18618",
+#                 "https://sabluximmobilier.com/page/2/?advanced_contystate=senegal&filter_search_action%5B0%5D=terrains&property_status&submit=Recherche&elementor_form_id=18618",
+#                 "https://sabluximmobilier.com/?advanced_contystate=senegal&filter_search_action%5B%5D=villa&property_status=&submit=Recherche&elementor_form_id=18618",
+#                 "https://sabluximmobilier.com/page/2/?advanced_contystate=senegal&filter_search_action%5B0%5D=villa&property_status&submit=Recherche&elementor_form_id=18618"]
+
+# guinee_conakry= ["https://sabluximmobilier.com/?advanced_contystate=guinee-conakry&filter_search_action%5B0%5D=appartements&property_status=&submit=Recherche&elementor_form_id=18618",
+#                 "https://sabluximmobilier.com/page/2/?advanced_contystate=guinee-conakry&filter_search_action%5B0%5D=appartements&property_status&submit=Recherche&elementor_form_id=18618",
+#                 "https://sabluximmobilier.com/?advanced_contystate=guinee-conakry&filter_search_action%5B0%5D=plateaux-de-bureau&property_status&submit=Recherche&elementor_form_id=18618",
+#                 "https://sabluximmobilier.com/page/2/?advanced_contystate=guinee-conakry&filter_search_action%5B0%5D=plateaux-de-bureau&property_status&submit=Recherche&elementor_form_id=18618",
+#                 "https://sabluximmobilier.com/?advanced_contystate=guinee-conakry&filter_search_action%5B%5D=terrains&property_status=&submit=Recherche&elementor_form_id=18618",
+#                 "https://sabluximmobilier.com/page/2/?advanced_contystate=guinee-conakry&filter_search_action%5B0%5D=terrains&property_status&submit=Recherche&elementor_form_id=18618",
+#                 "https://sabluximmobilier.com/?advanced_contystate=guinee-conakry&filter_search_action%5B%5D=villa&property_status=&submit=Recherche&elementor_form_id=18618",
+#                 "https://sabluximmobilier.com/page/2/?advanced_contystate=guinee-conakry&filter_search_action%5B0%5D=villa&property_status&submit=Recherche&elementor_form_id=18618"]
+
+
+# cote_divoire= ["https://sabluximmobilier.com/?advanced_contystate=cote-divoire&filter_search_action%5B0%5D=appartements&property_status=&submit=Recherche&elementor_form_id=18618",
+#                 "https://sabluximmobilier.com/page/2/?advanced_contystate=cote-divoire&filter_search_action%5B0%5D=appartements&property_status&submit=Recherche&elementor_form_id=18618",
+#                 "https://sabluximmobilier.com/?advanced_contystate=cote-divoire&filter_search_action%5B0%5D=plateaux-de-bureau&property_status&submit=Recherche&elementor_form_id=18618",
+#                 "https://sabluximmobilier.com/page/2/?advanced_contystate=cote-divoire&filter_search_action%5B0%5D=plateaux-de-bureau&property_status&submit=Recherche&elementor_form_id=18618",
+#                 "https://sabluximmobilier.com/?advanced_contystate=cote-divoire&filter_search_action%5B%5D=terrains&property_status=&submit=Recherche&elementor_form_id=18618",
+#                 "https://sabluximmobilier.com/page/2/?advanced_contystate=cote-divoire&filter_search_action%5B0%5D=terrains&property_status&submit=Recherche&elementor_form_id=18618",
+#                 "https://sabluximmobilier.com/?advanced_contystate=cote-divoire&filter_search_action%5B%5D=villa&property_status=&submit=Recherche&elementor_form_id=18618",
+#                 "https://sabluximmobilier.com/page/2/?advanced_contystate=cote-divoire&filter_search_action%5B0%5D=villa&property_status&submit=Recherche&elementor_form_id=18618"]
