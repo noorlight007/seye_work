d76d6275e00e98ee5a479044374f0425595bc222
noorlight007 <contactwnoor@gmail.com>
2024-12-26 01:01:25 +0600

    update

--- sc_web.py
+++ sc_web.py
@@ -28,7 +28,7 @@ def scrape_sablux_properties(url):
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
 
-    service = Service("chromedriver.exe")  # Update the path to your ChromeDriver
+    service = Service("/usr/local/bin/chromedriver")
     driver = webdriver.Chrome(service=service, options=chrome_options)
 
     # Open the webpage
